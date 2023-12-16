e926f3f03516c3209120deedfc828bd8d9676b7a
mateomaza <1mateomaza@gmail.com>
2023-12-16 17:26:29 -0500

    Fixed ReferenceError (from localStorage in server)

--- src/services/axios.instance.ts
+++ src/services/axios.instance.ts
@@ -2,9 +2,18 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "http://localhost:3001/api",
-  headers: {
-    Authorization: `Bearer ${localStorage.getItem("token")}`,
-  },
+});
+
+axiosInstance.interceptors.request.use(config => {
+  if (typeof window !== "undefined") {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+  return config;
+}, error => {
+  return Promise.reject(error);
 });
 
 axiosInstance.interceptors.response.use(
