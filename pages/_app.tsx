9e8bd94ada9e359fd863d33b4003980f1edcf49c
mateomaza <1mateomaza@gmail.com>
2023-12-22 12:01:02 -0500

    Implemented inactivity automatic logout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Provider } from "react-redux";
-import { store } from "@/redux/store";
+import { AppDispatch, store } from "@/redux/store";
 import { AppProps } from "next/app";
+import { useDispatch } from 'react-redux';
+import { logout } from "@/redux/thunks/auth.thunks";
+
+let inactivityTimer: NodeJS.Timeout | number;
+
+const startInactivityTimer = (dispatch: AppDispatch, timeout = 15 * 60 * 1000) => {
+  if (inactivityTimer) clearTimeout(inactivityTimer);
+  inactivityTimer = setTimeout(() => dispatch(logout()), timeout);
+};
+
+const resetInactivityTimer = (dispatch: AppDispatch, timeout = 15 * 60 * 1000) => {
+  startInactivityTimer(dispatch, timeout);
+};
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    const handleActivity = () => resetInactivityTimer(dispatch);
+    document.addEventListener("mousemove", handleActivity);
+    document.addEventListener("keypress", handleActivity);
+    startInactivityTimer(dispatch);
+    return () => {
+      document.removeEventListener("mousemove", handleActivity);
+      document.removeEventListener("keypress", handleActivity);
+      clearTimeout(inactivityTimer);
+    };
+  }, [dispatch]);
+
   return (
     <Provider store={store}>
       <Component {...pageProps} />
@@ -11,4 +38,4 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   );
 };
 
-export default MyApp
+export default MyApp;
