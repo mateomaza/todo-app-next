504705a089f2209e980a5bea3913a075b539b9bb
mateomaza <1mateomaza@gmail.com>
2023-12-22 15:24:19 -0500

    Fixed issue in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,35 +1,33 @@
 import React, { useEffect } from "react";
 import { Provider } from "react-redux";
-import { AppDispatch, store } from "@/redux/store";
+import { store } from "@/redux/store";
 import { AppProps } from "next/app";
-import { useDispatch } from 'react-redux';
 import { logout } from "@/redux/thunks/auth.thunks";
 
 let inactivityTimer: NodeJS.Timeout | number;
 
-const startInactivityTimer = (dispatch: AppDispatch, timeout = 15 * 60 * 1000) => {
+const startInactivityTimer = (timeout = 15 * 60 * 1000) => {
   if (inactivityTimer) clearTimeout(inactivityTimer);
-  inactivityTimer = setTimeout(() => dispatch(logout()), timeout);
+  inactivityTimer = setTimeout(() => store.dispatch(logout()), timeout);
 };
 
-const resetInactivityTimer = (dispatch: AppDispatch, timeout = 15 * 60 * 1000) => {
-  startInactivityTimer(dispatch, timeout);
+const resetInactivityTimer = (timeout = 15 * 60 * 1000) => {
+  startInactivityTimer(timeout);
 };
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
-    const handleActivity = () => resetInactivityTimer(dispatch);
+    const handleActivity = () => resetInactivityTimer();
     document.addEventListener("mousemove", handleActivity);
     document.addEventListener("keypress", handleActivity);
-    startInactivityTimer(dispatch);
+    startInactivityTimer();
     return () => {
       document.removeEventListener("mousemove", handleActivity);
       document.removeEventListener("keypress", handleActivity);
       clearTimeout(inactivityTimer);
     };
-  }, [dispatch]);
+  }, []);
 
   return (
     <Provider store={store}>
