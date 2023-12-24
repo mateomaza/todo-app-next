77dc71fdab444772950d56aec1d9406697cf3e6d
mateomaza <1mateomaza@gmail.com>
2023-12-24 15:28:23 -0500

    Initial load access_token check

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,8 @@ import React, { useEffect } from "react";
 import { Provider } from "react-redux";
 import { store } from "@/redux/store";
 import { AppProps } from "next/app";
-import { logout } from "@/redux/thunks/auth.thunks";
+import { logout, refresh } from "@/redux/thunks/auth.thunks";
+import { useRouter } from "next/router";
 
 let inactivityTimer: NodeJS.Timeout | number;
 
@@ -16,8 +17,22 @@ const resetInactivityTimer = (timeout = 15 * 60 * 1000) => {
 };
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  const router = useRouter();
+  const access_token = store.getState().auth.token;
 
   useEffect(() => {
+    if (!access_token) {
+      store
+        .dispatch(refresh())
+        .then((result) => {
+          if (!result.payload) {
+            router.push("/auth/login-page");
+          }
+        })
+        .catch(() => {
+          router.push("/auth/login-page");
+        });
+    }
     const handleActivity = () => resetInactivityTimer();
     document.addEventListener("mousemove", handleActivity);
     document.addEventListener("keypress", handleActivity);
@@ -27,7 +42,7 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
       document.removeEventListener("keypress", handleActivity);
       clearTimeout(inactivityTimer);
     };
-  }, []);
+  }, [access_token, router]);
 
   return (
     <Provider store={store}>
