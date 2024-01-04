8d4219d55aaa7f8001088267e50ea950ab152d59
mateomaza <1mateomaza@gmail.com>
2024-01-04 15:18:01 -0500

    Updated header & footer to use mui

--- src/app/nav/footer.tsx
+++ src/app/nav/footer.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
 const Footer = () => {
-  return <footer>© TaskTracker - 2023</footer>;
+  return (
+    <Box component="footer" sx={{ bgcolor: 'background.paper', py: 6 }}>
+      <Container maxWidth="lg">
+        <Typography variant="subtitle1" align="center" color="text.secondary" component="p">
+          © 2024 TaskTracker
+        </Typography>
+      </Container>
+    </Box>
+  );
 };
 
 export default Footer;
--- src/app/nav/header.tsx
+++ src/app/nav/header.tsx
@@ -1,13 +1,27 @@
-import React from "react";
-import Link from "next/link";
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from 'next/link';
+import Button from '@mui/material/Button';
 
 const Header = () => {
   return (
-    <header>
-      <nav>
-        <Link href="/">Home</Link>
-      </nav>
-    </header>
+    <AppBar position="static">
+      <Container maxWidth="lg">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            TaskTracker
+          </Typography>
+          <nav>
+            <Link href="/" passHref>
+              <Button color="inherit">Home</Button>
+            </Link>
+          </nav>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 };
 
