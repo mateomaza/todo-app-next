4f2bb923d2e543f9c20e73515f217ab7b3317d9a
mateomaza <1mateomaza@gmail.com>
2024-02-23 18:19:55 -0500

    Customized DatePicker

---
+++ src/app/custom/date.picker.tsx
@@ -0,0 +1,20 @@
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+interface MyDatePickerProps {
+  selected: Date | null;
+  onChange: (date: Date | null) => void;
+}
+
+export function MyDatePicker({ selected, onChange }: MyDatePickerProps) {
+  return (
+    <div style={{ border: "1px solid #4a90e2", display: "inline-block", padding: '3px' }}>
+      <DatePicker
+        selected={selected}
+        onChange={onChange}
+        showTimeSelect
+        dateFormat="Pp"
+      />
+    </div>
+  );
+}
