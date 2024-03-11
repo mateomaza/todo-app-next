8a997062761e916f928e12acb83c8a1e69d0f926
mateomaza <1mateomaza@gmail.com>
2024-03-11 15:22:04 -0500

    Updated baseURL so cookies matched the domain

--- src/services/axios.instance.ts
+++ src/services/axios.instance.ts
@@ -5,7 +5,7 @@ import { parseCookies } from 'nookies';
 import Router from "next/router";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://todo-app-api-production-2472.up.railway.app/api",
+  baseURL: "https://app.holi.website/api",
   withCredentials: true,
 });
 
