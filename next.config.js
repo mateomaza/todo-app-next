86882587d1c7ba65c6c2b262461b00659bc9e7bc
mateomaza <1mateomaza@gmail.com>
2024-03-21 13:21:48 -0500

    Webpack-bundle-analyzer + eslint error

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (process.env.ANALYZE) {
+      config.plugins.push(
+        new BundleAnalyzerPlugin({
+          analyzerMode: 'server',
+          analyzerPort: 'auto',
+          openAnalyzer: true,
+        })
+      );
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
