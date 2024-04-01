ca8001b45f40b75ca9ccfa0d6570e626138c952b
mateomaza <1mateomaza@gmail.com>
2024-04-01 16:29:25 -0500

    Enhanced header.tsx styling

--- src/app/nav/header.tsx
+++ src/app/nav/header.tsx
@@ -35,41 +35,44 @@ const Header = () => {
     }
   };
   return (
-    <div className="bg-white shadow-md w-full text-black font-bold h-[90px] fixed top-[0%] left-0 flex flex-row items-center px-6 py-3 z-50">
-      <Link href="/" passHref>
-        <div className="flex items-center">
-          <h2 className="text-[20px] smd:text-[16px] smd:w-[6rem] md:w-[13rem]">
-            Task Tracker
-          </h2>
-          <Image
-            src="/dalle-logo.png"
-            alt="Logo"
-            width={1024}
-            height={1024}
-            className="round-logo"
-          />
-        </div>
-      </Link>
-      <div className="flex flex-row w-full justify-end">
+    <div className="bg-white shadow-md w-full text-black font-bold h-[90px] fixed top-0 left-0 flex items-center px-6 py-3 z-50">
+      <div className="flex items-center justify-start w-1/3">
+        <Link href="/" passHref>
+          <a className="flex items-center">
+            <h2 className="text-[20px] smd:text-[16px] smd:w-[6rem] md:w-[13rem]">
+              Task Tracker
+            </h2>
+            <Image
+              src="/dalle-logo.png"
+              alt="Logo"
+              width={1024}
+              height={1024}
+              className="ml-1 round-logo"
+            />
+          </a>
+        </Link>
+      </div>
+      <div className="flex-grow text-center">
+        <p className="text-sm font-light">
+          App created by{" "}
+          <a
+            href="https://linktr.ee/mateomaza"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline"
+          >
+            <u>Mateo Maza</u>
+          </a>
+        </p>
+      </div>
+      <div className="flex items-center justify-end w-1/3">
         {errorMessage && (
-          <div className="my-3">
+          <div className="my-3 mr-4">
             <Error errorMessage={errorMessage} />
           </div>
         )}
-        <div className="text-sm font-light mr-4">
-          <p>
-            App created by{" "}
-            <a
-              href="https://linktr.ee/mateomaza"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <u>Mateo Maza</u>
-            </a>
-          </p>
-        </div>
         <LogoutButton />
-        <div className="flex flex-row items-center justify-center border-2 border-blue-600 hover:bg-blue-600 hover:text-white px-2 py-2 rounded-[4px] md:hidden  smd:ml-4 cursor-pointer">
+        <div className="flex flex-row items-center justify-center border-2 border-blue-600 hover:bg-blue-600 hover:text-white px-2 py-2 rounded-[4px] md:hidden smd:ml-4 cursor-pointer">
           <i
             className="fa-solid fa-user-slash text-[18px] text-gray-400 mr-3 smd:mr-0 hover:text-white"
             onClick={() => deleteButtonRef.current!.openModal()}
